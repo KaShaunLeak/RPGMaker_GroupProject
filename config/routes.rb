be1057a62750a28c7d598307ae35484109dcafdf
kashaun leak <kashaunleak@gmail.com>
2019-03-28 23:26:52 +0000

    Added additional css styling (trouble with getting characters.css to work

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     get 'application/index'
     resources :characters
+    get 'characters/index'
     root 'application#index'
 end
