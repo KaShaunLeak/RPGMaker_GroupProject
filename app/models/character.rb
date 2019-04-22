91d51d3632af60f4d8320199434859a8e834dbe4
kashaun leak <kashaunleak@gmail.com>
2019-04-22 18:52:57 +0000

    Slight Format on text fields; Added minimum requirement for 'name'

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,12 +1,3 @@
 class Character < ApplicationRecord
     validates :name, presence: true, length: {minimum: 5}
-    validates :sex, presence: true, length: {minimum: 1, maximum: 1}
-    validates :age, presence: true, length: {minimum: 1, maximum: 3}
-    validates :race, presence: true, length: {minimum: 3, maximum: 15}
-    validates :job, presence: true, length: {maximum: 15}
-    validates :birthday, presence: true, length: {maximum: 20}
-    validates :birth, presence: true, length: {maximum: 20}
-    validates :country, presence: true, length: {maximum: 20}
-    validates :personality, presence: true, length: {minimum: 0, maximum: 150}
-    validates :backstory, presence: true, length: {minimum: 0, maximum: 512}
 end
