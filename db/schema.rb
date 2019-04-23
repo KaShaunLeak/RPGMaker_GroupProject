2f1bdda50f4c4ca133e326595f52b5b6d1b3a220
EC2 Default User <ec2-user@ip-172-31-27-90.us-east-2.compute.internal>
2019-04-23 20:02:26 +0000

    Add users model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_22_173744) do
+ActiveRecord::Schema.define(version: 2019_04_23_195945) do
 
   create_table "characters", force: :cascade do |t|
     t.string "name"
@@ -30,6 +30,16 @@ ActiveRecord::Schema.define(version: 2019_04_22_173744) do
   end
 
   create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.string "name"
+    t.string "oauth_token"
+    t.datetime "oauth_expires_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
