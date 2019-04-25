2b128ec9428919f55f28af310fa9c6266eb4223c
Chris <cmalang@uncc.edu>
2019-04-25 20:08:03 +0000

    Added Comments functionality to character page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,12 @@
 class CommentsController < ApplicationController
     def create
-        @character = Character.find(params[:article_id])
+        @character = Character.find(params[:character_id])
         @comment = @character.comments.create(comment_params)
         redirect_to character_path(@character)
     end
     
-    private 
-    def comment_params
-        params.require(:comment).permit(:commenter, :body)
-    end
+    private
+        def comment_params
+            params.require(:comment).permit(:commenter, :body)
+        end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,3 @@
 class Comment < ApplicationRecord
-    belongs_to :character
+  belongs_to :character
 end
